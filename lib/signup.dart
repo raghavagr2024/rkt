0056f47ddf0bfeea737b447caae2b380d9b731f7
Armaan Sharma <30438481+ArmaanS211@users.noreply.github.com>
2023-07-31 15:52:05 -0500

    Update signup.dart

--- lib/signup.dart
+++ lib/signup.dart
@@ -17,7 +17,7 @@ class SignUpPage extends StatelessWidget {
       body: Container(
         decoration: const BoxDecoration(
           image:  DecorationImage(
-            image: AssetImage("../../../mainBackground2.jpeg"),
+            image: AssetImage("../../../lib/mainBackground2.jpeg"),
             fit: BoxFit.cover,
             opacity: 200
             )
